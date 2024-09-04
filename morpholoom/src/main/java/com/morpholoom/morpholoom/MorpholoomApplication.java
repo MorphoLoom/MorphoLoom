31babbdb5bea392a52a20843189ee59e814c1481
donggeun.kang <kdk7854@gmail.com>
2024-09-04 13:52:31 +0900

    Project init

---
+++ morpholoom/src/main/java/com/morpholoom/morpholoom/MorpholoomApplication.java
@@ -0,0 +1,13 @@
+package com.morpholoom.morpholoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorpholoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorpholoomApplication.class, args);
+	}
+
+}
---
+++ morpholoom/src/test/java/com/morpholoom/morpholoom/MorpholoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morpholoom.morpholoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpholoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
